efbbe043dcf32c444497f853b05dd1e9bd0200a4
vishal-hunashikatti <vishalb.hunashikatti@gmail.com>
2025-04-17 00:51:27 +0200

    library-management for personal use project initiated

---
+++ workspace-projects/library-management/src/library-management.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : library-management.c
+ Author      : vishal-hunashikatti
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
